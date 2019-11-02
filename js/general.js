0e26a33ec65ded9f946fd9dca242ca0d14d392f9
Diego Alejandro Aguilera Martinez <diego.aguilera@unillanos.edu.co>
2019-11-02 18:07:12 -0500

    Menú, nuevo logo, modal

--- js/general.js
+++ js/general.js
@@ -1,6 +1,8 @@
 const menu = document.querySelector('.menu-desktop')
 const menuButton = document.getElementById('menu-button')
+const menuHelper = document.getElementById('menu-helper')
 function abrirMenu(e) {
+  menuHelper.classList.toggle('mostrar-helper')
   menu.classList.toggle('mostrar')
 }
 menuButton.addEventListener('click', abrirMenu)
